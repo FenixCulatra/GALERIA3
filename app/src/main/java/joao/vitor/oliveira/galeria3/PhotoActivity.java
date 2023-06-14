b2c8319e95c4e6ba479e79139068c0821b621823
20211TIIMI0095 <joao.scheidegger@gmail.com>
2023-06-14 09:08:54 -0300

    Interface pronto

--- app/src/main/java/joao/vitor/oliveira/galeria3/PhotoActivity.java
+++ app/src/main/java/joao/vitor/oliveira/galeria3/PhotoActivity.java
@@ -1,8 +1,10 @@
 package joao.vitor.oliveira.galeria3;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class PhotoActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class PhotoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo);
+
+        //Setando a barra de ação
+        Toolbar toolbar = findViewById(R.id.tbPhoto);
+        setSupportActionBar(toolbar);
+        //habilitando o home button
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
     }
 }
