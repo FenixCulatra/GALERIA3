27c085916f30c948d2d266e334dac882247b0d2e
20211TIIMI0095 <joao.scheidegger@gmail.com>
2023-07-07 10:41:07 -0300

    V 1.2

---
+++ app/src/main/java/joao/vitor/oliveira/galeria3/MainViewHolder.java
@@ -0,0 +1,12 @@
+package joao.vitor.oliveira.galeria3;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MainViewHolder extends RecyclerView.ViewHolder {
+    public MainViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
--- app/src/main/java/joao/vitor/oliveira/galeria3/PhotoActivity.java
+++ app/src/main/java/joao/vitor/oliveira/galeria3/PhotoActivity.java
@@ -1,13 +1,26 @@
 package joao.vitor.oliveira.galeria3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.FileProvider;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
-import android.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+import java.io.File;
+
 
 public class PhotoActivity extends AppCompatActivity {
-
+    String photoPath;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,9 +29,43 @@ public class PhotoActivity extends AppCompatActivity {
         //Setando a barra de ação
         Toolbar toolbar = findViewById(R.id.tbPhoto);
         setSupportActionBar(toolbar);
-        //habilitando o home button
+        //Habilitando o home button
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
 
+        //Pega o intent, o caminho da foto e coloca no imageView
+        Intent i  = getIntent();
+        photoPath = i.getStringExtra("photo_path");
+        Bitmap bitmap = com.example.produtos.util.Util.getBitmap(photoPath);
+        ImageView imPhoto = findViewById(R.id.imPhoto);
+        imPhoto.setImageBitmap(bitmap);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        //Cria-se uma função que quando for criado as opções do menu, ele virá do arquivo que criei
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.photo_activity_tb, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.opShare:
+                sharePhoto();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    void sharePhoto() {
+        //Funcao que envia a foto
+        Uri photoUri = FileProvider.getUriForFile(PhotoActivity.this, "joao.vitor.oliveira.galeria3.fileprovider", new File(photoPath));
+        Intent i = new Intent(Intent.ACTION_SEND);
+        i.putExtra(Intent.EXTRA_STREAM, photoUri);
+        i.setType("image/jpeg");
+        startActivity(i);
     }
 }
